366a9f2d5ecfd84b948427126e8c490eb0c123c1
Paulo Junior <148215124+pauljr235@users.noreply.github.com>
2025-12-08 16:52:55 +0000

    Hero

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,6 @@
+import bannerImg from '../../assets/images/harryPotter.png'
+import { Banner } from './styles'
+
+const Hero = () => <Banner style={{backgroundImage: `url(${bannerImg})`}}></Banner>
+
+export default Hero
---
+++ src/components/Hero/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const Banner = styled.div`
+  display: block;
+  height: 480px;
+  width: 100%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 100%;
+`
